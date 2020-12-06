a3c447e2bdad6a18dce18eff2a40734d2a4a7d74
34ka <60933524+34ka@users.noreply.github.com>
2020-12-06 18:37:44 +0600

    assert with pdf files

---
+++ src/test/java/tests/files/PdfTests.java
@@ -0,0 +1,30 @@
+package tests.files;
+
+import com.codeborne.pdftest.PDF;
+import org.junit.jupiter.api.Test;
+import utils.ZipUtils;
+
+import java.io.File;
+import java.io.IOException;
+
+import static com.codeborne.selenide.Selenide.sleep;
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+import static utils.FileUtils.readStringFromFile;
+
+
+public class PdfTests {
+
+    @Test
+    void verifyContentInTxtTest() throws IOException {
+//Класс, который помогает считывать в файле информацию вспомогательный класс для считывания FileUtils.java
+        String actualFilePath = "src/test/resources/html/1.pdf";
+        String expectedFileText = "Центральная тема";
+
+        File actualFile = new File(actualFilePath);
+        PDF pdf = new PDF(actualFile);
+
+        assertThat(pdf, PDF.containsText(expectedFileText));
+
+    }
+}
